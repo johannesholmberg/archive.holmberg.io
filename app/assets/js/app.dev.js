b0df82423b152599b42ae62fe92f51b467aec0c8
Johannes Holmberg <johannes@holmberg.io>
2015-11-24 17:24:02 +0100

    Finishing up new masthead.

--- app/assets/js/app.dev.js
+++ app/assets/js/app.dev.js
@@ -1,28 +1,28 @@
-window.onload = function(){
+// window.onload = function(){
 
-  // Is this browser sufficiently modern to continue?
-  if ( ! ( "querySelector" in document 
-    && "addEventListener" in window 
-    && "getComputedStyle" in window ) ) {
-  return; 
-  }
+//   // Is this browser sufficiently modern to continue?
+//   if ( ! ( "querySelector" in document 
+//     && "addEventListener" in window 
+//     && "getComputedStyle" in window ) ) {
+//   return; 
+//   }
   
-  window.document.documentElement.className += " enhanced";
+//   window.document.documentElement.className += " enhanced";
 
-  var nav = document.querySelector( ".nav-primary ul" ),
-  navToggle = document.querySelector( ".js-mobile-menu-trigger" );
+//   var nav = document.querySelector( ".nav-primary ul" ),
+//   navToggle = document.querySelector( ".js-mobile-menu-trigger" );
 
-  if ( navToggle ) {
-    navToggle.addEventListener( "click",
-      function( e ) {
-      if ( nav.className == "open" ) {
-        nav.className = "";
-      } else {
-      nav.className = "open";
-      }
-      e.preventDefault();
-    }, false );
-  }
+//   if ( navToggle ) {
+//     navToggle.addEventListener( "click",
+//       function( e ) {
+//       if ( nav.className == "open" ) {
+//         nav.className = "";
+//       } else {
+//       nav.className = "open";
+//       }
+//       e.preventDefault();
+//     }, false );
+//   }
 
 
-}
+// }
