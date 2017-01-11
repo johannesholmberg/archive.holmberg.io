e48c4e2d0351e2b3c997bcb2fa81c05ff880092f
Johannes Holmberg <johannes@holmberg.io>
2017-01-11 09:26:49 +0100

    Add fragment identifier links to headings

--- source/assets/js/source/app.dev.js
+++ source/assets/js/source/app.dev.js
@@ -1,3 +1,5 @@
+
+// Disable hover events while scrolling the website
 var body = document.body, timer;
 window.addEventListener('scroll', function() {
   clearTimeout(timer);
@@ -8,3 +10,19 @@ window.addEventListener('scroll', function() {
     body.classList.remove('disable-hover')
   }, 100);
 }, false);
+
+
+var identifiers = document.querySelectorAll('.s-main-content h2, .s-main-content h3,  .s-main-content h4');
+
+identifiers.forEach(identifier => {
+  var id = identifier.id;
+  if (id) {
+    var link = `<a href="#${id}" class="fragment-identifier">
+                  <svg viewBox="0 0 96 96" class="icon" width="24" height="24">
+                    <use xlink:href="#link"></use>
+                  </svg>
+                </a>`;
+    identifier.innerHTML += link;
+  }
+
+});
